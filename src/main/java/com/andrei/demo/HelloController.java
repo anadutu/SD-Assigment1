d66c90852532ed13d095f84067998f54e9342e85
anadutu <dutuanasofia@gmail.com>
2025-03-25 11:09:26 +0200

    inital commit: added backend demo

---
+++ src/main/java/com/andrei/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.andrei.demo;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(HttpServletRequest request) {
+        return "Hello World! " + request.getSession().getId();
+    }
+}
